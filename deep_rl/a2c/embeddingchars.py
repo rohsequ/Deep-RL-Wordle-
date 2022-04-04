77789cf2307328ba9f0685e7b61d6f322b9220ca
kumaram123 <61126309+kumaram123@users.noreply.github.com>
2022-04-04 19:54:47 -0400

    Update embeddingchars.py

--- deep_rl/a2c/embeddingchars.py
+++ deep_rl/a2c/embeddingchars.py
@@ -12,6 +12,7 @@ class EmbeddingChars(nn.Module):
                  n_hidden: int = 1,
                  hidden_size: int = 256,
                  n_emb: int = 32,
+                 word_len_var: int,
                  ):
         """
         Args:
@@ -20,7 +21,7 @@ class EmbeddingChars(nn.Module):
             hidden_size: size of hidden layers
         """
         super().__init__()
-        word_width = 26*5
+        word_width = 26*word_len_var
         self.n_emb = n_emb
 
         layers = [
